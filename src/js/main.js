6fdd5d35b84393000c31cdee3194f07178b170a1
andreas - laptop <andreas.atakan@student.uib.no>
2018-10-26 13:36:35 +0200

    Final fixes to index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 13)
 
+// initializing basemap
 var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18
 }).addTo(map);
 
+// initializing marker
 var marker = L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
